5d799b13ef8704204e53e7a843720a7f66865203
HarleyVader <urukkai@outlook.com>
2024-10-22 00:37:19 +0200

    3.6.159

--- public/js/text2speech.js
+++ public/js/text2speech.js
@@ -17,10 +17,9 @@ function arrayPush(_audioArray, e) {
 function arrayShift(_audioArray) {
   if (_audioArray.length > 0 && audio !== null) {
     let _currentURL = _audioArray.shift();
-    //console.log("currentURL ", _currentURL);
+    console.log("currentURL ", _currentURL);
     return _currentURL;
   }
-  return null;
 }
 
 async function do_tts(_audioArray) {
@@ -28,13 +27,12 @@ async function do_tts(_audioArray) {
   document.querySelector("#message").textContent = "Synthesizing...";
 
   let currentURL = arrayShift(_audioArray);
-
   audio.src = currentURL;
   console.log("audio.src ", audio.src);
   audio.load();
   audio.onloadedmetadata = function () {
-    //console.log("audio ", audio);
-    //console.log("audio.duration ", audio.duration);
+    console.log("audio ", audio);
+    console.log("audio.duration ", audio.duration);
     document.querySelector("#message").textContent = "Playing...";
     audio.play();
   };
@@ -46,6 +44,5 @@ async function do_tts(_audioArray) {
   audio.onerror = function (e) {
     console.error("Error playing audio:", e);
     document.querySelector("#message").textContent = "Error playing audio.";
-    state = true;
   };
-}
+};
