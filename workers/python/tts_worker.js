1f4c496ddd4ec72df9bf022bb319b3b9a6f9e355
HarleyVader <urukkai@outlook.com>
2025-02-13 21:48:25 +0100

    1.1.1527

--- workers/python/tts_worker.js
+++ workers/python/tts_worker.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import os from 'os';
-import { colors, patterns } from '../middleware/bambisleepChalk.js';
+import { colors, patterns } from '../../middleware/bambisleepChalk.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
