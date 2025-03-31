4fe88d6b5c2a2f81ca2d32813e2074a2586c7eea
HarleyVader <urukkai@outlook.com>
2025-03-31 07:37:27 +0200

    help update

--- routes/psychodelic-trigger-mania.js
+++ routes/psychodelic-trigger-mania.js
@@ -3,16 +3,10 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const validConstantsCount = 9; // Define the variable with an appropriate value
-  if (data && data.bambiname) {
-    res.render('psychodelic-trigger-mania', { 
-  
-      validConstantsCount: validConstantsCount
-    });
-  } else {
-    res.render('psychodelic-trigger-mania', { 
-      validConstantsCount: validConstantsCount
-    });
-  }
+
+  res.render('psychodelic-trigger-mania', { 
+    validConstantsCount: validConstantsCount
+  });
 });
 
 export default router;
